73a0b68c39cefd1eded5ac445409aae1a1de44de
Ivanov <ivanov2012gen@yandex.ru>
2026-05-20 23:02:27 +0500

    docker: add netcat, remove hardcoded secrets, fix db healthcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+set -e
+
+while ! nc -z db 5432; do
+  echo 'Waiting for postgres...'
+  sleep 1
+done
+
+echo 'Postgres is up!'
 alembic upgrade head
-exec uvicorn api.main:app --host 0.0.0.0 --port 8000
+uvicorn api.main:app --host 0.0.0.0 --port 8000 --reload
