08d7971b8d6540e97c5eabfcee34d7778b918626
Евгений Иванов <ivanov2012gen@yandex.ru>
2026-08-19 23:21:58 +0500

    feat: add new test_transfer and new fixture in conftest

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -5,6 +5,7 @@ import uuid
 from api.main import app
 from api.database import engine
 from httpx import AsyncClient, ASGITransport
+from decimal import Decimal
 
 
 @pytest_asyncio.fixture
@@ -49,7 +50,7 @@ async def test_user(client):
 
 @pytest_asyncio.fixture
 async def auth_token(client, test_user):
-    """Фикстура создает пользователя и возвращает токен"""
+    """Фикстура авторизует пользователя и возвращает токен"""
     login_data = {"email": test_user["email"], "password": test_user["password"]}
     response = await client.post("/auth/login", json=login_data)
     assert response.status_code == 200
@@ -69,3 +70,48 @@ async def test_wallet(client, auth_headers):
     response = await client.post("/wallets/", json=wallet_data, headers=auth_headers)
     assert response.status_code == 200
     return response.json()["wallet_uuid"]
+
+
+@pytest_asyncio.fixture
+async def test_user2(client):
+    """Фикстура создает второго тестового пользователя и возвращает его данные"""
+    user_data = {
+        "first_name": "Test2",
+        "last_name": "User2",
+        "email": f"{uuid.uuid4()}@example.com",
+        "password": "secret123"
+    }
+    response = await client.post("/auth/register", json=user_data)
+    assert response.status_code == 200
+
+    return user_data
+
+
+@pytest_asyncio.fixture
+async def auth_token2(client, test_user2):
+    """Фикстура авторизует второго пользователя и возвращает токен"""
+    login_data = {"email": test_user2["email"], "password": test_user2["password"]}
+    response = await client.post("/auth/login", json=login_data)
+    assert response.status_code == 200
+    return response.json()["access_token"]
+
+
+@pytest_asyncio.fixture
+async def auth_headers2(auth_token2):
+    """Фикстура возвращает заголовок токена для второго пользователя"""
+    return {"Authorization": f"Bearer {auth_token2}"}
+
+
+@pytest_asyncio.fixture
+async def test_wallet2(client, auth_headers2):
+    """Создает тестовый кошелек для второго пользователя и возвращает его UUID"""
+    wallet_data = {"currency": "RUB"}
+    response = await client.post("/wallets/", json=wallet_data, headers=auth_headers2)
+    assert response.status_code == 200
+    return response.json()["wallet_uuid"]
+
+
+async def wallet_balance(client, wallet_uuid, headers):
+    response1 = await client.get(f"/wallets/{wallet_uuid}", headers=headers)
+    assert response1.status_code == 200
+    return Decimal(response1.json()["balance"])
---
+++ api/tests/test_transfer.py
@@ -0,0 +1,166 @@
+import asyncio
+import pytest
+import pytest_asyncio
+import uuid
+
+from api.database import AsyncSessionLocal
+from api.tests.conftest import wallet_balance
+from decimal import Decimal
+
+
+@pytest.mark.asyncio
+async def test_transfer_success(
+        client, test_wallet, auth_headers2,
+        test_wallet2, auth_headers
+):
+    """
+    Проверяем успешный перевод:
+    денежное списание с отправителя и зачисление получателю
+    """
+    wallet1_uuid = test_wallet
+
+    wallet2_uuid = test_wallet2
+
+    initial_balance1 = await wallet_balance(client, wallet1_uuid, auth_headers)
+    initial_balance2 = await wallet_balance(client, wallet2_uuid, auth_headers2)
+
+    transfer_data = {
+        "to_wallet_id": wallet2_uuid,
+        "amount": 30.00,
+        "currency": "RUB"
+    }
+    transfer_response = await client.post(
+        f"/wallets/{wallet1_uuid}/transfer",
+        json=transfer_data,
+        headers=auth_headers
+    )
+    assert transfer_response.status_code == 200
+
+    new_balance1 = await wallet_balance(client, wallet1_uuid, auth_headers)
+    new_balance2 = await wallet_balance(client, wallet2_uuid, auth_headers2)
+
+    assert new_balance1 == initial_balance1 - Decimal("30.00")
+    assert new_balance2 == initial_balance2 + Decimal("30.00")
+
+
+@pytest.mark.asyncio
+async def test_transfer_insufficient_funds(
+        client, test_wallet, auth_headers2,
+        test_wallet2, auth_headers
+):
+    """Недостаточно средств на балансе для перевода"""
+    wallet1_uuid = test_wallet
+
+    wallet2_uuid = test_wallet2
+
+    initial_balance1 = await wallet_balance(client, wallet1_uuid, auth_headers)
+    initial_balance2 = await wallet_balance(client, wallet2_uuid, auth_headers2)
+
+    transfer_data = {
+        "to_wallet_id": wallet2_uuid,
+        "amount": 3000.00,
+        "currency": "RUB"
+    }
+    transfer_response = await client.post(
+        f"/wallets/{wallet1_uuid}/transfer",
+        json=transfer_data,
+        headers=auth_headers
+    )
+    assert transfer_response.status_code == 400
+
+    new_balance1 = await wallet_balance(client, wallet1_uuid, auth_headers)
+    new_balance2 = await wallet_balance(client, wallet2_uuid, auth_headers2)
+
+    assert new_balance1 == initial_balance1
+    assert new_balance2 == initial_balance2
+
+
+@pytest.mark.asyncio
+async def test_transfer_recipient_not_found(client, test_wallet, auth_headers):
+    """Кошелек получателя не найден"""
+    wallet1_uuid = test_wallet
+
+    initial_balance1 = await wallet_balance(client, wallet1_uuid, auth_headers)
+
+    transfer_data = {
+        "to_wallet_id": str(uuid.uuid4()),
+        "amount": 50.00,
+        "currency": "RUB"
+    }
+    transfer_response = await client.post(
+        f"/wallets/{wallet1_uuid}/transfer",
+        json=transfer_data,
+        headers=auth_headers
+    )
+    assert transfer_response.status_code == 404
+
+    new_balance1 = await wallet_balance(client, wallet1_uuid, auth_headers)
+
+    assert new_balance1 == initial_balance1
+
+
+@pytest.mark.asyncio
+async def test_transfer_currency_mismatch(client, auth_headers, auth_headers2):
+    """Проверка несовпадение валюты кошельков не совпадают"""
+    wallet_data = {"currency": "RUB"}
+    response = await client.post("/wallets/", json=wallet_data, headers=auth_headers)
+    assert response.status_code == 200
+    wallet1_uuid = response.json()["wallet_uuid"]
+
+    wallet_data2 = {"currency": "USD"}
+    response2 = await client.post("/wallets/", json=wallet_data2, headers=auth_headers2)
+    assert response2.status_code == 200
+    wallet2_uuid = response2.json()["wallet_uuid"]
+
+    initial_balance1 = await wallet_balance(client, wallet1_uuid, auth_headers)
+    initial_balance2 = await wallet_balance(client, wallet2_uuid, auth_headers2)
+
+    transfer_data = {
+        "to_wallet_id": wallet2_uuid,
+        "amount": 30.00,
+        "currency": "RUB"
+    }
+    transfer_response = await client.post(
+        f"/wallets/{wallet1_uuid}/transfer",
+        json=transfer_data,
+        headers=auth_headers
+    )
+    assert transfer_response.status_code == 400
+
+    new_balance1 = await wallet_balance(client, wallet1_uuid, auth_headers)
+    new_balance2 = await wallet_balance(client, wallet2_uuid, auth_headers2)
+
+    assert new_balance1 == initial_balance1
+    assert new_balance2 == initial_balance2
+
+
+@pytest.mark.asyncio
+async def test_transfer_access_denied(
+        client, test_wallet, auth_headers2,
+        test_wallet2, auth_headers
+):
+    """Проверка попытки перевода с чужого кошелька"""
+    wallet1_uuid = test_wallet
+
+    wallet2_uuid = test_wallet2
+
+    initial_balance1 = await wallet_balance(client, wallet1_uuid, auth_headers)
+    initial_balance2 = await wallet_balance(client, wallet2_uuid, auth_headers2)
+
+    transfer_data = {
+        "to_wallet_id": wallet2_uuid,
+        "amount": 30.00,
+        "currency": "RUB"
+    }
+    transfer_response = await client.post(
+        f"/wallets/{wallet1_uuid}/transfer",
+        json=transfer_data,
+        headers=auth_headers2
+    )
+    assert transfer_response.status_code == 403
+
+    new_balance1 = await wallet_balance(client, wallet1_uuid, auth_headers)
+    new_balance2 = await wallet_balance(client, wallet2_uuid, auth_headers2)
+
+    assert new_balance1 == initial_balance1
+    assert new_balance2 == initial_balance2
